adf76c23c1a51c2423da9268acc3af373d0f1c14
Siddube <prajwal.be.21@gmail.com>
2024-05-28 09:58:55 +0530

    update SQL Table

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
 	title = models.CharField(max_length=250)
-	rating = models.IntegerField(default=1)
+	author = models.CharField(max_length=250, null=True)
+	rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+	is_best_seller = models.BooleanField(default=False)
 
+	def __str__(self):
+		return f'{self.title}, {self.author}, ({self.rating})' 
