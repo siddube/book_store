bc34d7be93e54b43eb67c875697cc4f74b98fefa
Siddube <prajwal.be.21@gmail.com>
2024-05-29 13:55:59 +0530

    fetch single book route and view from database

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Book
 # Create your views here.
 
@@ -6,3 +6,7 @@ def index(request):
   book = Book.objects.all()
   
   return render(request, 'book_outlet/index.html', {'books': book})
+
+def book_detail(request, id):
+  book = get_object_or_404(Book, pk=id)
+  return render(request, 'book_outlet/book_detail.html', {'book': book}) 
