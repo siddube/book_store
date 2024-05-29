1e60955b89bda02f7311abe6c8b17ddc122118a4
Siddube <prajwal.be.21@gmail.com>
2024-05-29 13:06:46 +0530

    fetch data from database

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
 
 def index(request):
-  return render(request, 'book_outlet/index.html')
+  book = Book.objects.all()
+  
+  return render(request, 'book_outlet/index.html', {'books': book})
