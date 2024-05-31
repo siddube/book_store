adf22193d65d845690d3c74aaf1bff57a326cea0
Siddube <prajwal.be.21@gmail.com>
2024-05-31 10:42:02 +0530

    add author model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class BookAdmin(admin.ModelAdmin):
   list_filter = ('author', 'rating')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,12 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+  name = models.CharField(max_length=250)
 
 class Book(models.Model):
 	title = models.CharField(max_length=250)
-	author = models.CharField(max_length=250, null=True)
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
 	rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 	is_best_seller = models.BooleanField(default=False)
 	slug = models.SlugField(default='', null=False, db_index=True)
